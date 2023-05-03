320216c380ae05c9a241b4a975493577b98292eb
TheNobody-12 <sarthak.kce20@sot.pdpu.ac.in>
2023-05-03 20:29:13 +0530

    Final Updates

---
+++ Support/TransformerEncoder.py
@@ -0,0 +1,55 @@
+import numpy as np
+from tensorflow import keras
+from tensorflow.keras import layers
+
+def to_sequences(SEQ_SIZE,obs):
+    x = []
+    y = []
+
+    for i in range(len(obs)-SEQ_SIZE):
+        window = obs[i:(i+SEQ_SIZE)]
+        after_window = obs[i+SEQ_SIZE]
+        window = [[x] for x in window]
+        x.append(window)
+        y.append(after_window)
+
+    return np.array(x),np.array(y)
+
+def transformer_encoder(inputs, head_size, num_heads, ff_dim, dropout=0):
+    # Normalization and Attention
+    x = layers.LayerNormalization(epsilon=1e-6)(inputs)
+    x = layers.MultiHeadAttention(
+        key_dim=head_size, num_heads=num_heads, dropout=dropout
+    )(x, x)
+    x = layers.Dropout(dropout)(x)
+    res = x + inputs
+
+    # Feed Forward Part
+    x = layers.LayerNormalization(epsilon=1e-6)(res)
+    x = layers.Conv1D(filters=ff_dim, kernel_size=1, activation="relu")(x)
+    x = layers.Dropout(dropout)(x)
+    x = layers.Conv1D(filters=inputs.shape[-1], kernel_size=1)(x)
+    return x + res
+
+
+def build_model(
+    input_shape,
+    head_size,
+    num_heads,
+    ff_dim,
+    num_transformer_blocks,
+    mlp_units,
+    dropout=0,
+    mlp_dropout=0,
+):
+    inputs = keras.Input(shape=input_shape)
+    x = inputs
+    for _ in range(num_transformer_blocks):
+        x = transformer_encoder(x, head_size, num_heads, ff_dim, dropout)
+
+    x = layers.GlobalAveragePooling1D(data_format="channels_first")(x)
+    for dim in mlp_units:
+        x = layers.Dense(dim, activation="relu")(x)
+        x = layers.Dropout(mlp_dropout)(x)
+    outputs = layers.Dense(1)(x)
+    return keras.Model(inputs, outputs)
